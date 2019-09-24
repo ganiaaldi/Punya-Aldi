e25876c3abdc16c98e0be49dd75d333e05990067
ganiaaldi <mrgad98@gmail.com>
2019-09-24 12:47:22 +0700

    Implement Splash Screen & Slide Screen

---
+++ app/src/main/java/com/aldi/punyaaldi/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.aldi.punyaaldi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aldi.punyaaldi.com.aldi.punyaaldi.slidepage.PrefManager
+import com.aldi.punyaaldi.com.aldi.punyaaldi.slidepage.SlideScreen
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_ShowSlider.setOnClickListener {
+            PrefManager(applicationContext).setLaunched(true)
+            startActivity(Intent(this, SlideScreen::class.java))
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/aldi/punyaaldi/SplashScreen.kt
@@ -0,0 +1,17 @@
+package com.aldi.punyaaldi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aldi.punyaaldi.com.aldi.punyaaldi.slidepage.SlideScreen
+
+
+class SplashScreen:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, SlideScreen::class.java))
+        finish()
+    }
+}
+
